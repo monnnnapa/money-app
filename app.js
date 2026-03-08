e06180e3312f3df8eb8dd82d97f2aefdc527deb1
Napasorn Khajornkiattamrong <91190676+monnnnapa@users.noreply.github.com>
2026-03-08 17:00:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const API = "https://script.google.com/macros/s/AKfycbxGX04yKUqOicvMUFIBS7pUvRSj2Cxs0CqBydCq002dPGlRN4Jz1bmHfvmcD4Fg6A64NQ/exec"
-
+const API = "https://script.google.com/macros/s/AKfycbwMkQZZRGh6ffE8yTgasdlIIGeIt9EeM4X5imtSuxKYOQQmZ-ceGxfxJsVExy14lHpjsg/exec"
 async function loadData(){
 
 const res = await fetch(API)
 const data = await res.json()
 
 let html = ""
+let balance = 0
+
+let food = 0
+let gas = 0
+let travel = 0
 
 data.forEach(item => {
 
+if(item.type === "income"){
+balance += Number(item.amount)
+}else{
+balance -= Number(item.amount)
+}
+
+if(item.category === "food") food += Number(item.amount)
+if(item.category === "gas") gas += Number(item.amount)
+if(item.category === "travel") travel += Number(item.amount)
+
 html += `
+
 <div class="item">
 
 <b>${item.note}</b><br>
@@ -23,14 +38,40 @@ ${item.category} - ${item.amount}
 </button>
 
 </div>
+
 `
 
 })
 
 document.getElementById("list").innerHTML = html
 
+document.getElementById("balance").innerText =
+"฿" + balance.toLocaleString()
+
+drawChart(food,gas,travel)
+
 }
 
+function drawChart(food,gas,travel){
+
+new Chart(document.getElementById("chart"),{
+
+type:"doughnut",
+
+data:{
+labels:["อาหาร","น้ำมัน","เที่ยว"],
+
+datasets:[{
+
+data:[food,gas,travel]
+
+}]
+
+}
+
+})
+
+}
 
 async function addItem(){
 
@@ -55,10 +96,13 @@ method:"POST",
 body:JSON.stringify(data)
 })
 
+document.getElementById("category").value=""
+document.getElementById("amount").value=""
+document.getElementById("note").value=""
+
 loadData()
 
 }
-
 
 async function deleteItem(id){
 
@@ -69,19 +113,5 @@ method:"DELETE"
 loadData()
 
 }
-let balance = 0
-
-data.forEach(item=>{
-
-if(item.type === "income"){
-balance += Number(item.amount)
-}else{
-balance -= Number(item.amount)
-}
-
-})
-
-document.getElementById("balance").innerText =
-"฿" + balance.toLocaleString()
 
 loadData()
